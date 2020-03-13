c40071183779d4e478da60cd024fd7fac588f789
Rustam <israr.ru@yandex.ru>
2020-03-14 00:40:25 +0700

    - Загрузка списка всех и верное выделение

--- app/src/main/java/com/spa/spa/AppAdapter.java
+++ app/src/main/java/com/spa/spa/AppAdapter.java
@@ -1,21 +1,33 @@
 package com.spa.spa;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AppAdapter extends ArrayAdapter {
   private List appList = null;
   private Context context;
   private PackageManager packageManager;
+  public static final String APP_PREFERENCES = "mysettings";
+  private ArrayList sample;
+  private SharedPreferences sp;
+  public static final String APP_PREFERENCES1 = "/data/data/com.spa.spa/shared_prefs/mysettings.xml";
+  private File file;
+  private ToggleButton settt;
 
   public AppAdapter(Context context, int resource,
                     List objects) {
@@ -44,24 +56,81 @@ public class AppAdapter extends ArrayAdapter {
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
     View view = convertView;
+    file = new File(APP_PREFERENCES1);
+    LayoutInflater layoutInflater = (LayoutInflater) context
+        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    view = layoutInflater.inflate(R.layout.list_item, null);
 
-    if(null == view) {
-      LayoutInflater layoutInflater = (LayoutInflater) context
-          .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      view = layoutInflater.inflate(R.layout.list_item, null);
+    sp = context.getSharedPreferences(APP_PREFERENCES,
+        Context.MODE_PRIVATE);
+    if (file.exists()) {
+      sample = new ArrayList(sp.getStringSet("Apps", null));
     }
-
     ApplicationInfo data = (ApplicationInfo) appList.get(position);
+    if (file.exists()) {
+      for (int a = 0; a <= (sample.size() - 1); a++) {
+        String string = (String) sample.get(a);
+        String[] parts = string.split("-");
+        String part1 = parts[0]; // 004
+        String part2 = parts[1]; // 034556
 
-    if(null != data) {
+        if (part1.contains(data.loadLabel(packageManager))) {
+          TextView appName = (TextView) view.findViewById(R.id.app_name);
+          TextView packageName = (TextView) view.findViewById(R.id.app_package);
+          ImageView iconView = (ImageView) view.findViewById(R.id.app_icon);
+          settt = (ToggleButton) view.findViewById(R.id.satt);
+          //holder.settt.setOnClickListener(new Apps());
+          settt.setTag(position);
+
+          appName.setText(data.loadLabel(packageManager));
+          packageName.setText(data.packageName);
+          iconView.setImageDrawable(data.loadIcon(packageManager));
+          appName.setTextColor(Color.RED);
+//            if (appName.getTextColors().equals(Color.RED)) {
+//
+//            } else {
+//              appName.setTextColor(Color.RED);
+//            }
+
+        } else {
+          TextView appName = (TextView) view.findViewById(R.id.app_name);
+          TextView packageName = (TextView) view.findViewById(R.id.app_package);
+          ImageView iconView = (ImageView) view.findViewById(R.id.app_icon);
+          settt = (ToggleButton) view.findViewById(R.id.satt);
+          //holder.settt.setOnClickListener(new Apps());
+          settt.setTag(position);
+
+          appName.setTextColor(Color.WHITE);
+          appName.setText(data.loadLabel(packageManager));
+          packageName.setText(data.packageName);
+          iconView.setImageDrawable(data.loadIcon(packageManager));
+        }
+      }
+      return view;
+    } else {
       TextView appName = (TextView) view.findViewById(R.id.app_name);
       TextView packageName = (TextView) view.findViewById(R.id.app_package);
       ImageView iconView = (ImageView) view.findViewById(R.id.app_icon);
+      settt = (ToggleButton) view.findViewById(R.id.satt);
+      //holder.settt.setOnClickListener(new Apps());
+      settt.setTag(position);
 
       appName.setText(data.loadLabel(packageManager));
       packageName.setText(data.packageName);
       iconView.setImageDrawable(data.loadIcon(packageManager));
     }
+    settt.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        if (settt.isChecked()) {
+          Log.d("Array Value", "POS" + "yes");
+        } else {
+          Log.d("Array Value", "POS" + "no");
+        }
+      }
+    });
     return view;
   }
 }
+
--- app/src/main/java/com/spa/spa/MainActivity.java
+++ app/src/main/java/com/spa/spa/MainActivity.java
@@ -1,22 +1,38 @@
 package com.spa.spa;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.app.NotificationManager;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.spa.spa.settings.Settingss;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends ListActivity {
 
   /**
    * Activity.
@@ -30,25 +46,55 @@ public class MainActivity extends AppCompatActivity {
   private ToggleButton servisesettingbut;
   private Button reloadService;
   private Button additisetting;
+  public static final String APP_PREFERENCES = "mysettings";
+  public static final String APP_PREFERENCES1 = "/data/data/com.spa.spa/shared_prefs/mysettings.xml";
+  private String appPreferencesName = "AppNamed";
+  private TextView appsav;
+  private SharedPreferences sp;
+  private String load;
 
+  private PackageManager packageManager = null;
+  private List applist = null;
+  private AppAdapter listadapter = null;
+  private File file;
+  private ArrayList appList;
 
 
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    getSupportActionBar().hide();
+    //getSupportActionBar().hide();
     notificationManager = (NotificationManager)
         getSystemService(Context.NOTIFICATION_SERVICE);
     dndState(notificationManager);
     mactivity = this;
     blackCurtainView = new BlackCurtainView();
 
-
+    sp = getSharedPreferences(APP_PREFERENCES,
+        Context.MODE_PRIVATE);
 
     servisesettingbut = (ToggleButton) findViewById(R.id.servisesettingbut);
     reloadService = (Button) findViewById(R.id.reloadService);
     additisetting = (Button) findViewById(R.id.additisetting);
+    appsav = (TextView) findViewById(R.id.appSave);
+    //appsav.setText(sp.getString("Apps", ""));
+
+    file = new File(APP_PREFERENCES1);
+
+    if (file.exists()) {
+      Set<String> set = new HashSet<String>(sp.getStringSet("Apps", null));
+      appsav.setText(String.valueOf(set));
+      ArrayList<String> sample = new ArrayList<String>(sp.getStringSet("Apps", null));
+      if (sample.size() > 0) {
+        for (int i = (sample.size() - 1); i >= 0; i--) {
+          Log.d("Array Value", "Array Value" + sample.get(i));
+        }
+      }
+    } else {
+      appsav.setText("false");
+    }
+
 
     if (MyService.isServiceCreated()) {
       //Запущен
@@ -103,6 +149,9 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 200);
       }
     }
+
+    packageManager = getPackageManager();
+    new LoadApplications().execute();
   }
 
 
@@ -171,6 +220,84 @@ public class MainActivity extends AppCompatActivity {
       Intent intent = new Intent(android.provider.Settings
           .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
       startActivity(intent);
+    }
+  }
+
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(l, v, position, id);
+
+    ApplicationInfo app = (ApplicationInfo) applist.get(position);
+
+    try {
+      Intent intent = packageManager.getLaunchIntentForPackage(app.packageName);
+
+      if (intent != null) {
+        startActivity(intent);
+      }
+    } catch (ActivityNotFoundException e) {
+      Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+    } catch (Exception e) {
+      Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+    }
+  }
+
+  private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+//    if (file.exists()) {
+      appList = new ArrayList();
+      ArrayList sample = new ArrayList(sp.getStringSet("Apps", null));
+      if (sample.size() > 0) {
+        for (int i = 0; i <= (sample.size() - 1); i++) {
+          String string = (String) sample.get(i);
+          String[] parts = string.split("-");
+          String part1 = parts[0]; // 004
+          String part2 = parts[1]; // 034556
+          load = part2;
+          for (ApplicationInfo info : list) {
+            try {
+              if (packageManager.getLaunchIntentForPackage(info.packageName) != null) {
+                String lap = info.packageName;
+                if (load.equals(lap)) {
+                  appList.add(info);
+                }
+              }
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      }
+      return appList;
+//    }
+//    return appList;
+  }
+
+  private class LoadApplications extends AsyncTask<Void, Void, Void> {
+
+    private ProgressDialog progress = null;
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+      applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+
+      listadapter = new AppAdapter(MainActivity.this, R.layout.list_item, applist);
+
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+      setListAdapter(listadapter);
+      progress.dismiss();
+      super.onPostExecute(result);
+    }
+
+    @Override
+    protected void onPreExecute() {
+      progress = ProgressDialog.show(MainActivity.this, null, "Loading apps info...");
+      super.onPreExecute();
     }
   }
 }
